9455c465d80df916291141735ebfbacbc139a7e6
Katie Lonsdale <katie952@gmail.com>
2023-04-21 10:43:47 -0600

    test: test that page links to new bd page

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "merchant bulk discounts index page" do
     @bd_1 = @bulk_discounts.first
   end
   
-  describe "as a merchant, when I visit my bulk discounts index page" do
+  describe "As a merchant, when I visit my bulk discounts index page" do
     it "lists all of my bulk discounts - their discount and their qty threshold" do
       @bulk_discounts.each do |bd|
         visit merchant_bulk_discounts_path(bd.merchant_id)
@@ -22,5 +22,20 @@ RSpec.describe "merchant bulk discounts index page" do
       end
       expect(current_path).to eq(merchant_bulk_discount_path(@bd_1.merchant_id, @bd_1.id))
     end
+    it "has a link to create a new discount" do
+      visit merchant_bulk_discounts_path(@bd_1.merchant_id)
+      within("#create-new") do
+        click_link("Create New Discount")
+      end
+      expect(current_path).to eq(new_merchant_bulk_discount_path(@bd_1.merchant_id))
+    end
   end
 end
+
+
+# Then I see a link to create a new discount
+# When I click this link
+# Then I am taken to a new page where I see a form to add a new bulk discount
+# When I fill in the form with valid data
+# Then I am redirected back to the bulk discount index
+# And I see my new bulk discount listed
