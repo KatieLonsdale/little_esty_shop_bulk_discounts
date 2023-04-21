aa9225df93776dffcf6db8654bd3c068725a5aca
Katie Lonsdale <katie952@gmail.com>
2023-04-20 20:55:35 -0600

    feat: add show index and before actions

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,5 +1,24 @@
 class BulkDiscountsController < ApplicationController
+  before_action :find_merchant, only: [:index]
+  before_action :find_bulk_discount, only: [:show]
+
   def index
-    @bulk_discounts = BulkDiscount.all
+    @bulk_discounts = @merchant.bulk_discounts
+  end
+
+  def show
+  end
+
+  private
+  def bulk_discount_params
+    params.permit(:quantity_threshold, :percentage_discount)
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def find_bulk_discount
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
 end
