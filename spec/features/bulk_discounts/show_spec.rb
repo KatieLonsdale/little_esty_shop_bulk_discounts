c4e2ca4e66b8120489584c08d2465c88b863b909
Katie Lonsdale <katie952@gmail.com>
2023-04-21 16:32:43 -0600

    test: for link to edit page

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe "Bulk discounts show page" do
   before(:each) do
     @merchant = create(:merchant)
     @bulk_discounts = create_list(:bulk_discount, 5, merchant_id: @merchant.id)
+    @bd_1 = @bulk_discounts.first
   end
+
   describe "As a merchant, when I visit my bulk discount show page" do
     it "shows me the bulk discount's quantity threshold and percentage discount" do
       @bulk_discounts.each do |bd|
@@ -15,4 +17,23 @@ RSpec.describe "Bulk discounts show page" do
       end
     end
   end
+
+  it "has a link to edit the bulk discount" do
+    visit merchant_bulk_discount_path(@merchant.id, @bd_1.id)
+    within("#edit") do
+      expect(page).to have_link("Edit")
+      click_link("Edit")
+    end
+
+    expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant.id, @bd_1.id))
+  end
 end
+
+
+# Then I see a link to edit the bulk discount
+# When I click this link
+# Then I am taken to a new page with a form to edit the discount
+# And I see that the discounts current attributes are pre-poluated in the form
+# When I change any/all of the information and click submit
+# Then I am redirected to the bulk discount's show page
+# And I see that the discount's attributes have been updated
