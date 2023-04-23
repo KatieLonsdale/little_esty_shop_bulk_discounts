9eb365e8d423f7c8cc43fb0975be7b84b91ebff5
Katie Lonsdale <katie952@gmail.com>
2023-04-23 13:18:40 -0600

    test: discount_applied method

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -40,4 +40,30 @@ RSpec.describe InvoiceItem, type: :model do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
     end
   end
+
+  describe "instance methods" do
+    before(:each) do
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+      @merchant2 = Merchant.create!(name: 'another merchant')
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_2 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+      @item_3 = Item.create!(name: "hoodie", description: "warm", unit_price: 20, merchant_id: @merchant2.id)
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2)
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
+      @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 1, unit_price: 10, status: 1)
+      @ii_3 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_3.id, quantity: 10, unit_price: 20, status: 1)
+      @bulk_discount1 = BulkDiscount.create!(percentage_discount: 50, quantity_threshold: 5, merchant_id: @merchant1.id)
+      @bulk_discount3 = BulkDiscount.create!(percentage_discount: 30, quantity_threshold: 5, merchant_id: @merchant1.id)
+      @bulk_discount2 = BulkDiscount.create!(percentage_discount: 25, quantity_threshold: 10, merchant_id: @merchant2.id)
+      @bulk_discount4 = BulkDiscount.create!(percentage_discount: 20, quantity_threshold: 10, merchant_id: @merchant2.id)
+    end
+    describe "discount_applied" do
+      it "returns the largest discount the invoice_item is eligible for" do
+        expect(@ii_1.discount_applied).to eq(@bulk_discount1)
+        expect(@ii_2.discount_applied).to eq(nil)
+        expect(@ii_3.discount_applied).to eq(@bulk_discount2)
+      end
+    end
+  end
 end
