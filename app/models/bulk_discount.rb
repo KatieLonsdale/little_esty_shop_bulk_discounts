1741337e05fd01022f21925f534e096facd12b01
Katie Lonsdale <katie952@gmail.com>
2023-04-25 17:44:06 -0600

    feat: add validation that percentage discount is less than 100

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,8 +1,5 @@
 class BulkDiscount < ApplicationRecord
-  validates :percentage_discount, 
-            :quantity_threshold, 
-            numericality: true, 
-            presence: true
+  validates :percentage_discount, numericality: { less_than_or_equal_to: 100}, presence: true
+  validates :quantity_threshold, numericality: true, presence: true
   belongs_to :merchant
-
 end
