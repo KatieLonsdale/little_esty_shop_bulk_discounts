19e13e60aff5f631fbe90e71ed74d1d6cf7cd550
Katie Lonsdale <katie952@gmail.com>
2023-04-25 17:43:34 -0600

    test: that percentage discount is less than or equal to 100

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe BulkDiscount do
   describe "validations" do
     it { should validate_presence_of :percentage_discount}
-    it { should validate_numericality_of :percentage_discount}
+    it { should validate_numericality_of(:percentage_discount).is_less_than_or_equal_to(100)}
     it { should validate_presence_of :quantity_threshold}
     it { should validate_numericality_of :quantity_threshold}
   end
